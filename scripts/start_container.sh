0aa6dd1e2dafb4a0f09d96781e987dc464e865d6
akhilamasna <55177150+akhilamasna@users.noreply.github.com>
2025-10-07 19:54:43 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull akhilamasna/sample-pyhton-project
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 akhilamasna/sample-pyhton-project
+docker run -d -p 5000:5000 akhilamasna/sample-pyhton-project 
